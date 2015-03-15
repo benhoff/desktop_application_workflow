1d887e8dcc35ee2b1153bcc4d73d0d628a6fc0d6
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-03-15 08:20:00 -0500

    Created our own QListModel

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 import sys
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 
+class MyListModel(QtCore.QAbstractListModel):
+    def __init__(self, *args, **kwargs):
+        super(MyListModel, self).__init__(*args, **kwargs)
+        self.data_ = ['Fluffy', 'Bunnies', 'Blue', 'purple']
 
+    def rowCount(self, parent=QtCore.QModelIndex):
+        return len(self.data_)
 
+    def data(self, index, role=QtCore.Qt.DisplayRole):
+        if not index.isValid():
+            return QtCore.QVariant()
 
+        if role != QtCore.Qt.DisplayRole:
+            return QtCore.QVariant()
+
+        return self.data_[index.row()]
+
+    def setData(self, index, value, role=QtCore.Qt.EditRole):
+        if not index.isValid() or role != QtCore.Qt.EditRole:
+            return False
+        self.data_[index.row()] = value
+        self.dataChanged.emit(index, index)
+        return True
+
+    def flags(self, index):
+        if index.isValid():
+            return QtCore.Qt.ItemIsEditable | QtCore.Qt.ItemIsEnabled
 
 if __name__ == '__main__':
 
@@ -11,15 +35,13 @@ if __name__ == '__main__':
 
     main_window = QtWidgets.QMainWindow()
     
-    list_widget = QtWidgets.QListWidget(parent=main_window)
-    simple_list = ['Butter', 'Blue', 'red', 'elephants']
+    my_list_model = MyListModel()
 
-    """
-    for item in simple_list:
-        list_widget.addItem(item)
-    """
-    list_widget.addItems(simple_list)
-    main_window.setCentralWidget(list_widget)
+    list_view = QtWidgets.QListView(parent=main_window)
+    list_view.setModel(my_list_model)
+
+
+    main_window.setCentralWidget(list_view)
     
     
     main_window.show()
