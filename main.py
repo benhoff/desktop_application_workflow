11b09d60d3c1bcd634aa3e9b0fc2a037f97de152
Ben Hoff <hoff.benjamin.k@gmail.com>
2015-03-15 07:48:53 -0500

    added a QListWidget as the central view

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import sys
 from PyQt5 import QtWidgets
 
-app = QtWidgets.QApplication(sys.argv)
 
-main_window = QtWidgets.QMainWindow()
-main_window.show()
 
-sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+
+    app = QtWidgets.QApplication(sys.argv)
+
+    main_window = QtWidgets.QMainWindow()
+    
+    list_widget = QtWidgets.QListWidget(parent=main_window)
+    simple_list = ['Butter', 'Blue', 'red', 'elephants']
+
+    for item in simple_list:
+        list_widget.addItem(item)
+
+    main_window.setCentralWidget(list_widget)
+    
+    
+    main_window.show()
+
+    sys.exit(app.exec_())
